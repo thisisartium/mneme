34bd53e53b6c02af3362e71c88a19c76811a3ec6
John Wilger <john@johnwilger.com>
2025-01-28 19:30:56 -0800

    Add noop execute function that always returns Ok

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,37 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use thiserror::Error;
+
+pub trait Command {}
+
+#[derive(Error, Debug)]
+pub enum ExecutionError {
+    #[error("Command execution failed: {0}")]
+    Unknown(String),
 }
 
+pub fn execute(_command: impl Command) -> Result<(), ExecutionError> {
+    Ok(())
+}
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    struct NoopCommand {
+        #[allow(dead_code)]
+        id: i32,
+    }
+    impl NoopCommand {
+        fn new(id: i32) -> Self {
+            NoopCommand { id }
+        }
+    }
+    impl Command for NoopCommand {}
+
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn successful_command_execution_with_no_events_produced() {
+        let command = NoopCommand::new(123);
+        match execute(command) {
+            Ok(()) => (),
+            other => panic!("Unexpected result: {:?}", other),
+        }
     }
 }
