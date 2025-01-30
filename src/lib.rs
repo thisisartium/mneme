a084a8b04ec52189a9810ef227d690ff72b2f7c4
John Wilger <john@johnwilger.com>
2025-01-29 19:22:29 -0800

    Use command's handle function in execution

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,24 @@
-use thiserror::Error;
+use std::error::Error;
 
-pub trait Command {}
-
-#[derive(Error, Debug)]
-pub enum ExecutionError {
-    #[error("Command execution failed: {0}")]
-    Unknown(String),
+pub trait Command<E: Error> {
+    fn handle(&self) -> Result<(), E>;
 }
 
-pub fn execute(_command: impl Command) -> Result<(), ExecutionError> {
-    Ok(())
+pub fn execute<E: Error>(command: impl Command<E>) -> Result<(), E> {
+    command.handle()
 }
+
 #[cfg(test)]
 mod tests {
+    use thiserror::Error;
+
     use super::*;
 
+    #[derive(Error, Debug)]
+    #[error("Execution error: {0}")]
+    struct ExecutionError(String);
+
     struct NoopCommand {
-        #[allow(dead_code)]
         id: i32,
     }
     impl NoopCommand {
@@ -24,13 +26,29 @@ mod tests {
             NoopCommand { id }
         }
     }
-    impl Command for NoopCommand {}
+    impl Command<ExecutionError> for NoopCommand {
+        fn handle(&self) -> Result<(), ExecutionError> {
+            match self.id {
+                456 => Err(ExecutionError("Rejected".to_string())),
+                _ => Ok(()),
+            }
+        }
+    }
 
     #[test]
     fn successful_command_execution_with_no_events_produced() {
         let command = NoopCommand::new(123);
         match execute(command) {
             Ok(()) => (),
+            other => panic!("Unexpected result: {:?}", other),
+        }
+    }
+
+    #[test]
+    fn command_rejection_error() {
+        let command = NoopCommand::new(456);
+        match execute(command) {
+            Err(ExecutionError(_)) => (),
             other => panic!("Unexpected result: {:?}", other),
         }
     }
